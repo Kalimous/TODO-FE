d226724ea89f25b431e64f8fceb23ad4012b4555
NEZ\DESKTOP <elsword485667@gmail.com>
2024-05-25 02:46:53 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import api from "./utils/api";
+import { useEffect, useState } from "react";
 
 import TodoBoard from "./components/TodoBoard";
 
@@ -8,24 +10,84 @@ import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
 
 function App() {
-  return (
-    <Container>
-      <Row className="add-item-row">
-        <Col xs={12} sm={10}>
-          <input
-            type="text"
-            placeholder="할일을 입력하세요"
-            className="input-box"
-          />
-        </Col>
-        <Col xs={12} sm={2}>
-          <button className="button-add">추가</button>
-        </Col>
-      </Row>
+    const [todoList, setTodoList] = useState([]);
+    const [todoValue, setTodoValue] = useState("");
 
-      <TodoBoard />
-    </Container>
-  );
+    const getTasks = async () => {
+        const response = await api.get("/tasks");
+        console.log("rrrr", response.data.data);
+        setTodoList(response.data.data);
+    };
+
+    const addTask = async () => {
+        try {
+            const response = await api.post("/tasks", {
+                task: todoValue,
+                isComplete: false,
+            });
+            if (response.status === 200) {
+                getTasks();
+                setTodoValue("");
+            }
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+    const deleteTask = async (id) => {
+        try {
+            const response = await api.delete(`tasks/${id}`);
+            console.log(response);
+            getTasks();
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+    const updateTask = async (id, task, isComplete) => {
+        try {
+            const response = await api.put(`tasks/${id}`, {
+                task: task,
+                isComplete: !isComplete,
+            });
+            getTasks();
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+    useEffect(() => {
+        getTasks();
+    }, []);
+
+    return (
+        <Container>
+            <Row className="add-item-row">
+                <Col xs={12} sm={10}>
+                    <input
+                        type="text"
+                        placeholder="할일을 입력하세요"
+                        className="input-box"
+                        value={todoValue}
+                        onChange={(evt) => {
+                            setTodoValue(evt.target.value);
+                        }}
+                    />
+                </Col>
+                <Col xs={12} sm={2}>
+                    <button className="button-add" onClick={addTask}>
+                        추가
+                    </button>
+                </Col>
+            </Row>
+
+            <TodoBoard
+                todoList={todoList}
+                deleteTask={deleteTask}
+                updateTask={updateTask}
+            />
+        </Container>
+    );
 }
 
 export default App;
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,21 +1,44 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
 
-const TodoItem = () => {
-  return (
-    <Row>
-      <Col xs={12}>
-        <div className={`todo-item`}>
-          <div className="todo-content">밥먹기</div>
+const TodoItem = ({ item, deleteTask, updateTask }) => {
+    return (
+        <Row>
+            <Col xs={12}>
+                <div
+                    className={`todo-item`}
+                    style={{
+                        backgroundColor: item.isComplete ? "gray" : "",
+                    }}
+                >
+                    <div className="todo-content">{item.task}</div>
 
-          <div>
-            <button className="button-delete">삭제</button>
-            <button className="button-delete">끝남</button>
-          </div>
-        </div>
-      </Col>
-    </Row>
-  );
+                    <div>
+                        <button
+                            className="button-delete"
+                            onClick={() => {
+                                deleteTask(item._id);
+                            }}
+                        >
+                            삭제
+                        </button>
+                        <button
+                            className="button-delete"
+                            onClick={() => {
+                                updateTask(
+                                    item._id,
+                                    item.task,
+                                    item.isComplete
+                                );
+                            }}
+                        >
+                            끝남
+                        </button>
+                    </div>
+                </div>
+            </Col>
+        </Row>
+    );
 };
 
 export default TodoItem;
